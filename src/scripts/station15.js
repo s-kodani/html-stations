36c45994d8c62e39005eadad74efa3c10ce9ae5c
Shinnosuke Kodani <kocchi.jan12@gmail.com>
2022-02-26 13:26:08 +0900

    station15

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -3,14 +3,35 @@ async function getData() {
   // ３秒後にフルネームのリストを表示されるようにしましょう。
   // 最初から表示されていると、クリアになりません。
   const userList = [
-    { id: 1, first_name: '優', family_name: '大木', affilication: 'TechTrain', is_student: false },
-    { id: 2, first_name: '太郎', family_name: '山田', affilication: 'HogeHoge大学', is_student: true }
+    {
+      id: 1,
+      first_name: "優",
+      family_name: "大木",
+      affilication: "TechTrain",
+      is_student: false,
+    },
+    {
+      id: 2,
+      first_name: "太郎",
+      family_name: "山田",
+      affilication: "HogeHoge大学",
+      is_student: true,
+    },
   ];
-  const result = await test()
-  return await result
+
+  // 3秒待機
+  const wait = sec => new Promise(resolve => setTimeout(resolve, sec * 1000));
+  await wait(3)
+
+  const result = test(userList);
+  return await result;
 }
 
-function test() {
-  return
+function test(userList) {
+  userList = userList.map((data) => {
+    data.full_name = data.family_name + " " + data.first_name;
+    return data
+  });
+  const wait = ms => new Promise(resolve => setTimeout(resolve, ms));
+  return userList;
 }
-
